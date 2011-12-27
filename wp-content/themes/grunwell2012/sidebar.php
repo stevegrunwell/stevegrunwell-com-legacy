aec79a5c3ed4d01a12e464e2b8dd193dc57adbcc
Steve Grunwell <stevegrunwell@gmail.com>
2011-12-27 16:14:03 -0500

    Wrapped the tweets in div#tweets

--- wp-content/themes/grunwell2012/sidebar.php
+++ wp-content/themes/grunwell2012/sidebar.php
@@ -10,18 +10,20 @@
 
 <div class="secondary" role="complementary">
   
-  <h3>Latest Tweets</h3>
-  <p>Follow me: <a href="#">@stevegrunwell</a></p>
+  <div id="twitter-module" class="module">
+    <h3>Latest Tweets</h3>
+    <p>Follow me: <a href="#">@stevegrunwell</a></p>
 
-  <ul class="tweets">
-  <?php for( $i=0; $i<3; $i++ ): ?>
+    <ul class="tweets">
+    <?php for( $i=0; $i<3; $i++ ): ?>
 
-    <li class="tweet">
-      This is an example Tweet. It was pulled in from my public timeline
-      <small>1 day ago</small>
-    </li>
+      <li class="tweet">
+        This is an example Tweet. It was pulled in from my public timeline
+        <small>1 day ago</small>
+      </li>
 
-  <?php endfor; ?>
-  </ul>
+    <?php endfor; ?>
+    </ul>
+  </div><!-- // twitter module -->
 
 </div><!-- // .secondary -->
