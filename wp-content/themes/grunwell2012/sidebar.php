4a20c6eeeaf492303780251274885d02846bda9f
Steve Grunwell <stevegrunwell@gmail.com>
2012-02-04 22:21:05 -0500

    Added a list of recent tweets to sidebar using Simple Twitter Timeline

--- wp-content/themes/grunwell2012/sidebar.php
+++ wp-content/themes/grunwell2012/sidebar.php
@@ -1,28 +1,27 @@
 <?php
 /**
- * Basic page template
+ * Basic page sidebar
  * @package WordPress
  * @subpackage grunwell2012
  * @author Steve Grunwell <steve@stevegrunwell.com>
  */
-
 ?>
 
 <div class="secondary" role="complementary">
-  
+
   <div id="twitter-module" class="module">
     <h3>Latest Tweets</h3>
-    <p>Follow me: <a href="#">@stevegrunwell</a></p>
+    <p>Follow me: <a href="https://twitter.com/#!/stevegrunwell" title="Follow @SteveGrunwell on Twitter" rel="external">@stevegrunwell</a></p>
 
     <ul class="tweets">
-    <?php for( $i=0; $i<3; $i++ ): ?>
+    <?php foreach( grunwell_get_tweets() as $tweet ): ?>
 
       <li class="tweet">
-        This is an example Tweet. It was pulled in from my public timeline
-        <small>1 day ago</small>
+        <?php echo $tweet['text']; ?>
+        <time><?php echo date('M jS @ g:ia', strtotime($tweet['created_at'])); ?></time>
       </li>
 
-    <?php endfor; ?>
+    <?php endforeach; ?>
     </ul>
   </div><!-- // twitter module -->
 
