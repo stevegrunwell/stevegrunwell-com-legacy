6962de3e1aa0ae5452cb9d37f12083b800b8f604
Steve Grunwell <stevegrunwell@gmail.com>
2012-02-18 17:36:00 -0500

    Added a (very plain) 404 page template

---
+++ wp-content/themes/grunwell2012/404.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 404 Page template
+ * @package WordPress
+ * @subpackage grunwell2012
+ * @author Steve Grunwell <steve@stevegrunwell.com>
+ */
+
+get_header(); ?>
+
+<div class="primary" role="main">
+
+  <h1>Page not found</h1>
+  <p>The page you were looking for could not be found.</p>
+
+</div><!-- // #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
