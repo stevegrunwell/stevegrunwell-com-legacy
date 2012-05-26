0a7f3cf61932e222a7faf257adc9cbc75037d72a
Steve Grunwell <stevegrunwell@gmail.com>
2012-05-26 13:08:35 -0400

    Collapse #primary-nav to a select menu when the screen is <= 500px wide

--- wp-content/themes/grunwell2012/js/main.js
+++ wp-content/themes/grunwell2012/js/main.js
@@ -21,11 +21,42 @@ function setContentHeight(){
   }
 }
 
+/**
+ * Create a select menu from a series of links, using the links' href attributes as the option value attribute
+ * @param mixed links A selector or jQuery object containing the links
+ * @return jQuery object
+ */
+function navToSelectMenu(links){
+  "use strict";
+  var select = jQuery('<select />');
+
+  jQuery('<option />', {
+    text: 'Go to...',
+    value: ''
+  }).appendTo(select);
+
+  jQuery(links).find('a').each(function(){
+    var self = jQuery(this);
+    jQuery('<option />', {
+      selected: ( self.parent('li').hasClass('current_page_parent') ? 'selected' : false ),
+      text: self.text(),
+      value: self.attr('href')
+    }).appendTo(select);
+  });
+  return select;
+}
+
 jQuery(function($){
 
   /** If .secondary is taller than .primary while 770px < window size < 1060px .secondary runs out of #content */
   setContentHeight();
   $(window).resize(setContentHeight);
+
+  /* Collapse #primary-nav into a <select> menu for smaller screens */
+  $('#primary-nav').append(navToSelectMenu('#primary-nav'));
+  $('#primary-nav').on('change', 'select', function(){
+    window.location = $(this).find('option:selected').val();
+  });
 
   /** Add placeholder support for older browsers */
   if( $.fn.placeholder ){
