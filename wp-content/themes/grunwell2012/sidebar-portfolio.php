45985937bcfc1f97d0ce872e084897618668e3ac
Steve Grunwell <stevegrunwell@gmail.com>
2012-05-28 17:32:21 -0400

    Single portfolio template; .btn styles

---
+++ wp-content/themes/grunwell2012/sidebar-portfolio.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Portfolio page sidebar
+ * @package WordPress
+ * @subpackage grunwell2012
+ * @author Steve Grunwell <steve@stevegrunwell.com>
+ */
+?>
+
+<div class="secondary" role="complementary">
+
+  <?php grunwell_custom_field('sidebar_content', null, '' ); ?>
+
+</div><!-- .secondary -->
---
+++ wp-content/themes/grunwell2012/single-grunwell_portfolio.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * A single portfolio entry
+ * @package WordPress
+ * @subpackage grunwell2012
+ * @author Steve Grunwell <steve@stevegrunwell.com>
+ */
+
+get_header(); ?>
+
+<?php while( have_posts() ): the_post(); ?>
+
+  <article id="post-<?php the_ID(); ?>" <?php post_class('primary'); ?> role="main">
+    <h1 class="post-title"><?php the_title(); ?></h1>
+
+    <?php the_content(); ?>
+
+  </article><!--// #post-<?php the_ID(); ?>-->
+
+<?php endwhile; ?>
+
+<?php get_sidebar('portfolio'); ?>
+<?php get_footer(); ?>
