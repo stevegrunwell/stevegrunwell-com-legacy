af39ef33dafd54b62ee7cfa84d5a522317463094
Steve Grunwell <stevegrunwell@gmail.com>
2012-07-19 22:23:59 -0400

    Added template specifically for /portfolio/

---
+++ wp-content/themes/grunwell2012/page-portfolio.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Template for /portfolio/
+ * @package WordPress
+ * @subpackage grunwell2012
+ * @author Steve Grunwell <steve@stevegrunwell.com>
+ */
+
+get_header(); ?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+
+  <div class="primary" role="main">
+    <h1 class="post-title"><?php grunwell_custom_field( 'alternate_headline', $post->ID, get_the_title() ); ?></h1>
+
+    <?php the_content(); ?>
+
+    <?php
+      $args = array(
+        'post_type' => 'grunwell_portfolio',
+      );
+    ?>
+  <?php foreach ( get_posts( $args ) as $post ): setup_postdata( $post ); ?>
+
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <?php if ( has_post_thumbnail() ) : ?>
+        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'medium', array( 'class' => 'alignright' ) ); ?></a>
+      <?php endif; ?>
+      <?php the_excerpt(); ?>
+      <?php echo get_the_tag_list( '<p>Tags: ', ', ', '</p>' ); ?>
+    </article><!-- #post-<?php the_ID(); ?> -->
+
+  <?php endforeach; ?>
+
+  </div><!-- .primary -->
+
+<?php endwhile; ?>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
