8ae264d0e990967a08cc59c19acee281e2af103b
Steve Grunwell <stevegrunwell@gmail.com>
2012-07-23 22:26:27 -0400

    Use post_date_gmt, not post_date in the sidebar

--- wp-content/themes/grunwell2012/sidebar.php
+++ wp-content/themes/grunwell2012/sidebar.php
@@ -14,7 +14,7 @@
 
   <h2>Latest Blog Posts</h2>
   <ul class="recent-posts">
-  <?php foreach ( $recent_posts as $recent ) : $time = strtotime( $recent['post_date'] ); ?>
+  <?php foreach ( $recent_posts as $recent ) : $time = strtotime( $recent['post_date_gmt'] ); ?>
     <?php printf( '<li><a href="%s">%s</a> <time datetime="%s" title="%s">%s ago</time></li>', get_permalink( $recent['ID'] ), $recent['post_title'], date( 'Y-m-d H:i:s', $time ), date( 'M jS, Y @ g:ia', $time ), human_time_diff( $time, time() ) ); ?>
   <?php endforeach; ?>
   </ul>
