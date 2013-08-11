f55ef433fb2de1f09a6eb5eda7286837d7824782
Steve Grunwell <stevegrunwell@gmail.com>
2013-08-11 14:01:11 -0400

    Prepare the site for Capistrano deployment

---
+++ wp-deploy.rb
@@ -0,0 +1,48 @@
+# WordPress Capistrano deployment
+
+## basic setup stuff ##
+
+# http://help.github.com/deploy-with-capistrano/
+set :application, "SteveGrunwell.com"
+set :repository, "git@github.com:stevegrunwell/stevegrunwell-com.git"
+set :scm, "git"
+default_run_options[:pty] = true
+
+# use our keys, make sure we grab submodules, try to keep a remote cache
+set :ssh_options, { :forward_agent => true }
+set :deploy_via, :checkout
+set :use_sudo, false
+set :git_enable_submodules, false
+
+set :branch, 'master'
+set :branch, $1 if `git branch` =~ /\* (\S+)\s/m
+
+## multi-stage deploy process ##
+
+# `cap production deploy`
+task :production do
+  set :user, "steve"
+  role :web, "stevegrunwell.com", :primary => true
+  set :app_environment, "production"
+  set :branch, "master"
+  set :keep_releases, 5
+  set :deploy_to, "/var/www/vhosts/stevegrunwell.com/httpdocs"
+end
+
+namespace :deploy do
+
+  task :finalize_update, :except => { :no_release => true } do
+    transaction do
+      run "chmod -R g+w #{releases_path}/#{release_name}"
+      run "ln -s #{shared_path}/uploads #{release_path}/wp-content/uploads"
+      run "ln -s #{shared_path}/wp-config.php #{release_path}/wp-config.php"
+      
+      # Grunwell 2012 actions
+      run "sass #{release_path}/wp-content/themes/grunwell2012/css/style.scss:#{release_path}/wp-content/themes/grunwell2012/css/style.css"
+      run "sass #{release_path}/wp-content/themes/grunwell2012/css/ie8.scss:#{release_path}/wp-content/themes/grunwell2012/css/ie8.css"
+    end
+  end
+
+  after "deploy", "deploy:cleanup"
+
+end
